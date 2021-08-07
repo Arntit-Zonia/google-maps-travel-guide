e4d3b8da29029c6b660f443b8a0ce1e8e4908529
Arntit <arditzonia@gmail.com>
2021-08-07 16:24:46 +0100

    Installed styling dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+
 import Header from "./components/header/Header";
 import ListOfLocations from "./components/list-of-locations/ListOfLocations";
 import LocationInfo from "./components/location-info/LocationInfo";
@@ -7,10 +9,16 @@ import Map from "./components/map/Map";
 const App = () => {
     return (
         <div className="app">
+            <CssBaseline />
             <Header />
-            <ListOfLocations />
-            <LocationInfo />
-            <Map />
+            <Grid container spacing={3} style={{ width: "100%" }}>
+                <Grid item xs={12} md={4}>
+                    <ListOfLocations />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
         </div>
     );
 };
